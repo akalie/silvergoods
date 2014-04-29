56ece23e8f1a8fa24f6e7a82ae841dcef565055f
akalie <askaslie@gmail.com>
2014-04-29 15:38:19 +0400

    shame

---
+++ ajax/order.php
@@ -0,0 +1,36 @@
+<?php
+/* Sites-Stroy.ru by iProger */
+
+function SendMail($em_to,$subject,$mess)
+{
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+	$headers .= 'To: '.$em_to . "\r\n";
+	$headers .= 'From: StarMaster <noreply@starsmaster.net>' . "\r\n";
+	
+	mail($em_to, $subject, $mess, $headers);
+}
+
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$tel = $_POST['tel'];
+$adress = $_POST['adress'];
+$kolvo = $_POST['kolvo'];
+$num = $_POST['num'];
+
+$adminEmail = 'a.krylov@topface.com';
+
+$mess = "Новый заказ Серебряной ложки.<br/>Ложка №".$num." <br/>ФИО: ".$fio."<br/>"."E-mail: ".$email."<br/>"."<br/>Телефон: ".$tel."<br/>Адрес: ".$adress;
+
+SendMail($adminEmail, 'Новый заказ', $mess);
+
+$mess = <<<HDO
+Уважаемый $fio <br/>
+Вы заказали у нас серебряную ложку ($kolvo шт.), указали адрес $adress, в течение рабочего дня с Вами свяжется наш менеджер для подтверждения заказа.<br/>
+Возникшие вопросы вы можете задать по такому-то номеру телефона.<br/>
+Спасибо, что воспользовались нашими услугами!
+HDO;
+
+SendMail($email, 'Ваш заказ', $mess);
+
+?>
---
+++ index.php
@@ -0,0 +1,373 @@
+<?php header ("Content-Type: text/html; charset=utf-8"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- Sites-Stroy.ru by iProger -->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Серебряные ложки</title>
+	<link media="all" rel="stylesheet" type="text/css" href="css/style.css" />
+	<link media="all" rel="stylesheet" type="text/css" href="css/jquery.countdown.css" />
+	<link media="all" rel="stylesheet" type="text/css" href="css/gallery.css" />
+	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+	<script src="js/jquery.countdown.js" type="text/javascript"></script>
+	<script src="js/main.js" type="text/javascript"></script>
+    <LINK rel="icon" href="images/favicon.png" type="image/x-icon">
+</head>
+<body>
+	<div class=wrapper>
+		<div class=header>
+            <div class=phone>
+                <!--Наш телефон<br/><br/>
+                (499) 704-39-82-->
+            </div>
+			<div class=main>
+				<div class=htext>
+					<img src="images/logo.png" /><br/>
+					Серебряные ложки от известного ювелира Проворова
+Идеальный подарок — серебряные ложки 925 пробы по cпециальной самой низкой цене в интернете!
+				</div>
+				
+				<div class=block-exclusive>
+					<div class=ex-head></div>
+					<div class=ex-body>
+						<div class=stock-price></div>
+						<small>Заполните пожалуйста поля</small>
+						<form class=sval-form method=POST>
+						<input name=fio type=text sval="ФИО" value="ФИО"/>
+						<input name=email type=text sval="E-mail" value="E-mail"/>
+						<input name=tel type=text sval="Телефон" value="Телефон"/>
+						<input name=adress type=text sval="Адрес (индекс, город и т. д.)" value="Адрес (индекс, город и т. д.)"/>
+						<input name=kolvo type=text sval="Количество" value="Количество"/>
+						<select name=number type=text sval="№ ложки" value="№ ложки">
+                            <option>Ложка №1</option>
+                            <option>Ложка №2</option>
+							<option>Ложка №3</option>
+							<option>Ложка №4</option>
+							<option>Ложка №5</option>
+							<option>Ложка №6</option>
+							<option>Ложка №7</option>
+                        </select>
+						<button class="buy">КУПИТЬ</button>
+						</form>
+						<br/>
+					</div>
+				</div><br/><br/><br/><br/><br/><br/>
+				</div>
+			</div>
+		</div>
+		<div class=stock-time>
+			<div class=main>
+				<div class=banner>
+					<img src="images/banner-time.png" /><br/>
+					<span>Осталось 17 ложек со скидкой</span>
+					
+				</div>
+				<div class=timer>
+					<div id="countdown"></div>
+					<div class="days">Дни</div>
+					<div class="hours">Часы</div>
+					<div class="mins">Минуты</div>
+					<div class="secs">Секунды</div>
+					<div class=clear></div>
+				</div>
+				<div class=clear></div>
+			</div>
+		</div>
+		<div class=features>
+			<div class=main>
+				<div class=feat>
+					<div class=fimg><img src="images/feat-1.png"/></div>
+					<div class=ftext>
+						<h3>Незаменимый подарок для детей и взрослых!</h3>
+						Её можно подарить как ребёнку (например, на крещение), так и взрослому в качестве сервиза.
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=feat>
+					<div class=fimg><img src="images/feat-2.png"/></div>
+					<div class=ftext>
+						<h3>Вы «убиваете» сразу двух «зайцев»</h3>
+						Она используется в качестве подарка или памятного сувенира, а так же обладает свойством долго сохранять воду свежей и чистой, обеззараживая ее.
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=feat>
+					<div class=fimg><img src="images/feat-3.png"/></div>
+					<div class=ftext>
+						<h3>Подарок на века</h3>
+						<br/>
+						Серебро никогда не потемнеет и не утратит первоначальный вид. Такой подарок запомнится на всю жизнь!
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=clear></div>
+			</div>
+		</div>
+		<div class=about>
+			<div class=main>
+				<h1>Серебряные ложки</h1>
+				
+				<div class=info>
+					7 серебряных ложек с оригинальным литьём на любой вкус. У каждого изделия есть клеймо изготовителя и Государственной Инспекции Пробирного Надзора. 
+Только 100% качество и гарантия!<br/><br/>
+					<div class=gallery style="margin-bottom:20px;">
+						<div class=gallery_container>
+							<img src="images/1.jpg" class='gallery_main animation in'>
+						</div>
+						<div class=gallery_thumbnails>
+							<div class=gallery_thumbnail><img src="images/2.jpg"></div>
+							<div class=gallery_thumbnail><img src="images/3.jpg"></div>
+							<div class=gallery_thumbnail><img src="images/4.jpg"></div>
+                            <div class=gallery_thumbnail><img src="images/5.jpg"></div>
+							<div class=gallery_thumbnail><img src="images/6.jpg"></div>
+							<div class=gallery_thumbnail><img src="images/7.jpg"></div>
+						</div>				
+					</div>
+				</div>
+				<div class=clear></div><br/>
+				<center><a href="#buy"><button class=bynow>Купить сейчас</button></a></center>
+			</div>
+		</div>
+		<div class=benefits>
+			<div class=main>
+				<h1>Преимущества серебряных ложек:</h1>
+				<div class=center>
+					<ul style="margin-right:180px;">
+						<li>Уникальный подарок для малыша<br/><br/></li>
+						<li>Украшение любого чаепития<br/><br/></li>
+						<li>Польза для здоровья детям и взрослым</li>
+					</ul>
+					<ul>
+						<li>Никогда не утратит своего блеска<br/><br/></li>
+						<li>Чистая питьевая вода без фильтров</li>
+						<li>Серебро - хорошая инвестиция<br/><br/></li>
+					</ul>
+					<div class=clear></div>
+					<div class=rarrows></div>
+					<img src="images/ok.png" align="left" />
+					<div class=oktext>
+						<h2>Серебряная ложка - отличный подарок себе и близким!</h2>
+						Это не только красивая, но и практичная вещь, которая будет служить вам долгие годы!
+					</div>
+					<div class=clear></div>
+ 				</div>
+				<br/>
+				<center><a href="#buy"><button class=bynow>Купить сейчас</button></a></center>
+			</div>
+		</div>
+		<div class=reviews>
+			<div class=main>
+				<h1>Отзывы наших счастливых клиентов</h1>
+				<div class=review>
+					<div class=avatar><img src="images/ava-1.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Артем Ильин</strong><br/>
+							<small>г. Смоленск</small>
+						</div>
+						Друзья пригласили нас на крещение своего третьего ребенка. Нужно было что-то подарить, мне пришла идея подарить малышу 
+						серебряную ложку, ведь это хорошая традиция в России. А зная родителей, я был уверен, что и они будут рады.
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=review>
+					<div class=avatar><img src="images/ava-2.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Сергей Калинин</strong><br/>
+							<small>г. Калининград</small>
+						</div>
+						Люблю питерских ювелиров за качество. Заказал себе 3 ложки для дома - в семье теперь никто не болеет. Красиво и полезно.
+					</div>
+					<div class=clear></div>
+				</div>
+								<div class=review>
+					<div class=avatar><img src="images/ava-3.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Олег Рыжков</strong><br/>
+							<small>г. Казань</small>
+						</div>
+						Покупал в подарок для мамы. Я ей целый сервиз составил из серебра :) Но что привлекло - цены низкие, серебро высокого качества.
+					</div>
+					<div class=clear></div>
+				</div>
+				<div id=hide-rev style="display:none;">
+				<div class=review>
+					<div class=avatar><img src="images/ava-4.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Сергей Маслов</strong><br/>
+							<small>г. Подольск </small>
+						</div>
+						Доставка работает быстро. Подарили ребёнку на первый зубик и довольны.
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=review>
+					<div class=avatar><img src="images/ava-5.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Лилия Живцева</strong><br/>
+							<small>г. Тобольск</small>
+						</div>
+						Заказала в довесок к золотым колечкам. Раньше в магазине ювелирки работала - так вот, могу сказать, что серебро качественное. Берите - не пожалеете! 
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=review>
+					<div class=avatar><img src="images/ava-6.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Павел Иванов</strong><br/>
+							<small>г. Казань</small>
+						</div>
+                       До Казани посылка дошла за 7 дней. Теперь ем йогурт по утрам вашей ложкой) 
+					</div>
+					<div class=clear></div>
+				</div>
+								<div class=review>
+					<div class=avatar><img src="images/ava-7.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Леонид Дмитриев</strong><br/>
+							<small>г. Брянск</small>
+						</div>
+                         Скажите, а как можно купить сразу оптом?
+					</div>
+					<div class=clear></div>
+				</div>
+								<div class=review>
+					<div class=avatar><img src="images/ava-8.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Алиса Мишина</strong><br/>
+							<small>г. Сочи</small>
+						</div>
+                       Вот только получила посылочку. Серебро блестит и сверкает, мыть очень легко. Спасибо!
+					</div>
+					<div class=clear></div>
+				</div>
+				</div>
+				<span onclick="showRev(this);" style="cursor:pointer; color:#049fbd;">Показать ещё >></span>
+			</div>
+		</div>
+		<div class=our-benefits>
+			<div class=main>
+				<h1>Наши преимущества</h1>
+				<div class="center2">
+				<div class=oben>
+					<img src="images/garant.png" />
+					<span class=obname>100% гарантия возврата денег.</span>
+					<div class=obdesc>Товар устроит вас или мы вернем вам деньги!</div>
+				</div>
+				<div class=oben>
+					<img src="images/bmap.png" />
+					<span class=obname>Доставка по всей России</span>
+					<div class=obdesc>Доставка во все города России.
+					Доставка курьером и пункты
+					самовывоза по всей Москве.</div>
+				</div>
+				<div class=oben style="margin-right:0px;">
+					<img src="images/econom.png" />
+					<span class=obname>Низкие цены</span>
+					<div class=obdesc>Мы работаем напрямую с производителем,
+					поэтому можем позволить себе акции и
+					низкие цены.</div>
+				</div>
+				<div class=clear></div>
+				</div>
+			</div>
+		</div>
+		<div class=howjob>
+			<div class=main>
+				<h1>Как мы работаем</h1>
+				<center>
+					<div class=steps>
+						<div class=step1>
+							<span>Вы оставляете заявку</span><br/>
+							Вы оставляете заявку на сайте или по телефону
+						</div>
+						<div class=step2>
+							<span>Мы доставляем Вашу посылку</span><br/>
+							Мы быстро доставляем Вашу посылку по указанному адресу
+						</div>
+						<div class=step3>
+							<span>Наш менеджер связывается с вами</span><br/>
+							Менеджер связывается с Вами для <br/>подтверждения заказа
+						</div>
+						<div class=step4>
+							<span>Вы получаете свой товар</span><br/>
+							Вы получаете свой товар и платите <br/>за него на почте или курьеру
+						</div>
+					</div>
+				</center>
+			</div>
+		</div>
+		<div id=buy class=footer>
+		    <div class=main>
+				<center>
+				<h1>Купи серебряную ложку сегодня!</h1><br/><br/>	
+				<form class=sval-form method=POST>
+				<input name=fio type=text sval="ФИО" value="ФИО" />
+				<input name=email type=text sval="E-mail" value="E-mail"/>
+				<input name=tel type=text sval="Телефон" value="Телефон" />
+				<input name=adress type=text sval="Адрес (индекс, город и т. д.)" value="Адрес (индекс, город и т. д.)"/>
+				<input name=kolvo type=text sval="Количество" value="Количество"/>
+				<select name=number type=text sval="№ ложки" value="№ ложки">
+                            <option>Ложка №1</option>
+                            <option>Ложка №2</option>
+							<option>Ложка №3</option>
+							<option>Ложка №4</option>
+							<option>Ложка №5</option>
+							<option>Ложка №6</option>
+							<option>Ложка №7</option>
+                        </select>
+				<button class="buy">Купить сейчас</button>
+				</form>
+				</center>
+			</div>
+		</div>
+		<center class=copy>2014 &copy; <a href="http://sites-stroy.ru/" target=_blank>Sites-Stroy.ru - создание и сопровождение сайтов.</a></center>
+	</div>
+	<div id="suc_order" class="modal_window"> 
+		<div class="close"></div>
+		<p>Мы отправили на ваш email письмо с данными заказа, если вы его там не нашли - посмотрите в спаме.</p>
+	</div>
+	<div id="scroller" class="b-top" style="display: none;"><span class="b-top-but"></span></div>
+   <div class="metrics" style="display: none">
+        <a href="https://metrika.yandex.ru/stat/?id=24618374&amp;from=informer"
+           target="_blank" rel="nofollow"><img src="//bs.yandex.ru/informer/24618374/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+                                               style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:24618374,lang:'ru'});return false}catch(e){}"/></a>
+        <!-- /Yandex.Metrika informer -->
+
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript">
+            (function (d, w, c) {
+                (w[c] = w[c] || []).push(function() {
+                    try {
+                        w.yaCounter24618374 = new Ya.Metrika({id:24618374,
+                            webvisor:true,
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true});
+                    } catch(e) { }
+                });
+
+                var n = d.getElementsByTagName("script")[0],
+                    s = d.createElement("script"),
+                    f = function () { n.parentNode.insertBefore(s, n); };
+                s.type = "text/javascript";
+                s.async = true;
+                s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+                if (w.opera == "[object Opera]") {
+                    d.addEventListener("DOMContentLoaded", f, false);
+                } else { f(); }
+            })(document, window, "yandex_metrika_callbacks");
+        </script>
+        <noscript><div><img src="//mc.yandex.ru/watch/24618374" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
+   </div>
+</body>
+</html>
