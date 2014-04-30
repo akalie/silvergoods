aa3aa28e5d4a911809d8b9fab80a728e578065c7
evgeeek <evgeeek@gmail.com>
2014-04-30 11:16:40 +0400

    Добавил телефон, исправил почту и заголовок письма

--- ajax/order.php
+++ ajax/order.php
@@ -6,7 +6,7 @@ function SendMail($em_to,$subject,$mess)
 	$headers  = 'MIME-Version: 1.0' . "\r\n";
 	$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 	$headers .= 'To: '.$em_to . "\r\n";
-	$headers .= 'From: StarMaster <noreply@starsmaster.net>' . "\r\n";
+	$headers .= 'From: StarMaster <noreply@silvergoods.net>' . "\r\n";
 	
 	mail($em_to, $subject, $mess, $headers);
 }
@@ -18,11 +18,11 @@ $adress = $_POST['adress'];
 $kolvo = $_POST['kolvo'];
 $num = $_POST['num'];
 
-$adminEmail = 'a.krylov@topface.com';
+$adminEmail = 'starsmaster@allsocial.ru';
 
 $mess = "Новый заказ Серебряной ложки.<br/>Ложка №".$num." <br/>ФИО: ".$fio."<br/>"."E-mail: ".$email."<br/>"."<br/>Телефон: ".$tel."<br/>Адрес: ".$adress;
 
-SendMail($adminEmail, 'Новый заказ', $mess);
+SendMail($adminEmail, 'Новый заказ Серебряной ложки', $mess);
 
 $mess = <<<HDO
 Уважаемый $fio <br/>
