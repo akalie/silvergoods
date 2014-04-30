8feaec655efd8658ebadc3acd136f0dce0507a50
akalie <askaslie@gmail.com>
2014-04-30 17:11:07 +0400

    добавил такой-то телефон, поменял автора письма

--- ajax/order.php
+++ ajax/order.php
@@ -3,12 +3,12 @@
 
 function SendMail($em_to,$subject,$mess)
 {
-	$headers  = 'MIME-Version: 1.0' . "\r\n";
-	$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-	$headers .= 'To: '.$em_to . "\r\n";
-	$headers .= 'From: StarMaster <noreply@silvergoods.net>' . "\r\n";
-	
-	mail($em_to, $subject, $mess, $headers);
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+    $headers .= 'To: '.$em_to . "\r\n";
+    $headers .= 'From: SilverGoods <noreply@silvergoods.net>' . "\r\n";
+
+    mail($em_to, $subject, $mess, $headers);
 }
 
 $fio = $_POST['fio'];
@@ -17,6 +17,7 @@ $tel = $_POST['tel'];
 $adress = $_POST['adress'];
 $kolvo = $_POST['kolvo'];
 $num = $_POST['num'];
+$phone = '+7(909)588-64-58';
 
 $adminEmail = 'starsmaster@allsocial.ru';
 
@@ -27,10 +28,8 @@ SendMail($adminEmail, 'Новый заказ Серебряной ложки', $
 $mess = <<<HDO
 Уважаемый $fio <br/>
 Вы заказали у нас серебряную ложку ($kolvo шт.), указали адрес $adress, в течение рабочего дня с Вами свяжется наш менеджер для подтверждения заказа.<br/>
-Возникшие вопросы вы можете задать по такому-то номеру телефона.<br/>
+Возникшие вопросы вы можете задать по телефону $phone .<br/>
 Спасибо, что воспользовались нашими услугами!
 HDO;
 
 SendMail($email, 'Ваш заказ', $mess);
-
-?>
