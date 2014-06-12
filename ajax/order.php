aadafbd035253330a090566652e472fcbd0663ab
akalie <askaslie@gmail.com>
2014-06-12 20:35:19 +0400

    first

---
+++ ajax/order.php
@@ -0,0 +1,40 @@
+<?php
+/* Sites-Stroy.ru by iProger */
+
+function SendMail($em_to,$subject,$mess)
+{
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+    $headers .= 'To: '.$em_to . "\r\n";
+    $headers .= 'From: SilverGoods <noreply@silvergoods.ru>' . "\r\n";
+
+    mail($em_to, $subject, $mess, $headers);
+}
+
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$tel = $_POST['tel'];
+$adress = $_POST['adress'];
+$kolvo = $_POST['kolvo'];
+$num = $_POST['num'];
+$phone = '+7(909)588-64-58';
+$logFilePath = 'ordersLog.txt';
+$handle = fopen($logFilePath, 'a');
+
+$adminEmail = 'starsmaster@allsocial.ru';
+
+$mess = "Новый заказ Серебряной ложки.<br/>Ложка №".$num." в количестве $kolvo <br/>ФИО: ".$fio."<br/>"."E-mail: ".$email."<br/>"."<br/>Телефон: ".$tel."<br/>Адрес: ".$adress;
+
+fwrite($handle, $mess . PHP_EOL);
+fclose($handle);
+
+SendMail($adminEmail, 'Новый заказ Серебряной ложки', $mess);
+
+$mess = <<<HDO
+Уважаемый $fio <br/>
+Вы заказали у нас серебряную ложку ($kolvo шт.), указали адрес $adress, в течение рабочего дня с Вами свяжется наш менеджер для подтверждения заказа.<br/>
+Возникшие вопросы вы можете задать по телефону $phone .<br/>
+Спасибо, что воспользовались нашими услугами!
+HDO;
+
+SendMail($email, 'Ваш заказ', $mess);
